ac6c67a641eb404549c54793c4137c2bd70694a0
George King <me@gking.uk>
2021-03-29 13:09:46 +0100

    Added iOS project

---
+++ ios/MinerStatsForEthermine/MinerStatsForEthermine/MinerStatsForEthermineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinerStatsForEthermineApp.swift
+//  MinerStatsForEthermine
+//
+//  Created by George King on 29/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct MinerStatsForEthermineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/MinerStatsForEthermine/MinerStatsForEthermineTests/MinerStatsForEthermineTests.swift
@@ -0,0 +1,33 @@
+//
+//  MinerStatsForEthermineTests.swift
+//  MinerStatsForEthermineTests
+//
+//  Created by George King on 29/03/2021.
+//
+
+import XCTest
+@testable import MinerStatsForEthermine
+
+class MinerStatsForEthermineTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
