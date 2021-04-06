fdd929cc3e7d9724aa810ccbcd45d1c0e70e783b
George King <me@gking.uk>
2021-04-06 20:12:36 +0100

    Not mining messages

--- web/index.php
+++ web/index.php
@@ -26,7 +26,11 @@
 
 		<div class="col-md-3">
 			<h2>CURRENT RATE</h2>
-			<h3>&Xi;<?=number_format($stat['mseday'],5)?> (<?=$fiat['sym'].number_format(($stat['mseday']*$ethtofiat),2)?>)</h3>
+			<?php if ($stat['hashrate'] == 0) { ?>
+				<h3>NOT MINING! :(</h3>
+			<?php } else { ?>
+				<h3>&Xi;<?=number_format($stat['mseday'],5)?> (<?=$fiat['sym'].number_format(($stat['mseday']*$ethtofiat),2)?>)</h3>
+			<?php } ?>
 		</div>
 		<div class="col-md-3">
 			<h2>EST TODAY</h2>
@@ -48,14 +52,19 @@
 
 		<div id="showDiv">
 			<div class="col-md-4">
-				<ul class="list-group">
-					<li class="list-group-item list-group-item-<?=$conf['colour']?>"><h4><a href=<?="https://my.minerstat.com/worker/".$conf['ms_worker_name']?> target="_blank">Minerstat</a></h4></li>
-					<li class="list-group-item">Hashrate	<span class="pull-right"><?=number_format($stat['hashrate'],2)?> MH/s @ <?=$stat['power']?>W</span></li>
-					<li class="list-group-item">Efficiency	<span class="pull-right"><?=$stat['accepted']?>/<?=$stat['rejected']?> (<?=number_format(100 - (($stat['rejected'] / $stat['accepted']) * 100), 2)?>%)</span></li>
-					<li class="list-group-item">Temperature	<span class="pull-right"><?=$stat['temp']?>°C</span></li>
-					<li class="list-group-item">Fan Speed	<span class="pull-right"><?=$stat['fanspeed']?>%</span></li>
-					<li class="list-group-item">Uptime	<span class="pull-right"><?=$stat['uptime']?></span></li>
-				</ul>
+				<?php if ($stat['hashrate'] == 0) { ?>
+					<h1 style="font-size: 48px;">NOT CURRENTLY MINING</h1>
+				<?php }
+				else { ?>
+					<ul class="list-group">
+						<li class="list-group-item list-group-item-<?=$conf['colour']?>"><h4><a href=<?="https://my.minerstat.com/worker/".$conf['ms_worker_name']?> target="_blank">Minerstat</a></h4></li>
+						<li class="list-group-item">Hashrate	<span class="pull-right"><?=number_format($stat['hashrate'],2)?> MH/s @ <?=$stat['power']?>W</span></li>
+						<li class="list-group-item">Efficiency	<span class="pull-right"><?=$stat['accepted']?>/<?=$stat['rejected']?> (<?=number_format(100 - (($stat['rejected'] / $stat['accepted']) * 100), 2)?>%)</span></li>
+						<li class="list-group-item">Temperature	<span class="pull-right"><?=$stat['temp']?>°C</span></li>
+						<li class="list-group-item">Fan Speed	<span class="pull-right"><?=$stat['fanspeed']?>%</span></li>
+						<li class="list-group-item">Uptime	<span class="pull-right"><?=$stat['uptime']?></span></li>
+					</ul>
+				<?php } ?>
 			</div>
 
 			<div class="col-md-4">
